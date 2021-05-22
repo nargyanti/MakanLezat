413cc466f99340ebf40868ef0574cb336f00addd
Nabilah Argyanti <nargyanti@gmail.com>
2021-05-23 03:22:01 +0700

    Submission 1

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,46 @@
 import 'regenerator-runtime'; /* for async await transpile */
 import '../styles/main.css';
+import restaurants from '../DATA.js'
 
-console.log('Hello Coders! :)');
+const hamburgerButtonElement = document.querySelector("#hamburger");
+const drawerElement = document.querySelector("#drawer");
+const mainElement = document.querySelector("main");
+const jumbotronElement = document.querySelector(".jumbotron");
+
+hamburgerButtonElement.addEventListener("click", event => {
+    drawerElement.classList.toggle("open");
+    event.stopPropagation();
+});
+
+mainElement.addEventListener("click", event => {
+    drawerElement.classList.remove("open");
+    event.stopPropagation();
+});
+
+jumbotronElement.addEventListener("click", event => {
+    drawerElement.classList.remove("open");
+    event.stopPropagation();
+});
+
+const renderRestaurantListItem = function () {
+    console.log(restaurants)
+    const restaurantListItem = document.querySelector(".restaurant-list");
+    restaurantListItem.innerHTML = "";
+    restaurants.forEach(restaurant => {
+        restaurantListItem.innerHTML += `
+        <article class="restaurant-list__item">
+            <div class="restaurant-list__item__header">
+                <h2>${restaurant.name}</h2>
+                <p class="rating">&#9733; ${restaurant.rating}</p>
+                <img src="${restaurant.pictureId}" alt="Foto restoran ${restaurant.name}">
+            </div>
+            <div class="restaurant-list__item__body">
+                <p class="location">Lokasi: ${restaurant.city}</p>
+                <p class="restaurant-list__item__body__caption">${restaurant.description}</p>
+            </div>
+        </article>
+        `;
+    });
+}
+
+document.addEventListener("DOMContentLoaded", renderRestaurantListItem);
