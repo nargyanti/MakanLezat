f01ebc02620d6b7fd50c32080e729b36aaff8040
Nabilah Argyanti <nargyanti@gmail.com>
2021-07-25 22:12:14 +0700

    Submission 3: edit space codecept

--- codecept.conf.js
+++ codecept.conf.js
@@ -5,31 +5,31 @@ const { setHeadlessWhen } = require('@codeceptjs/configure');
 setHeadlessWhen(process.env.HEADLESS);
 
 exports.config = {
-  tests: 'e2e/**/*.spec.js',
-  output: 'e2e/outputs',
-  helpers: {
-    Puppeteer: {
-      url: 'http://localhost:8080',
-      show: true,
-      windowSize: '1200x900'
-    }
-  },
-  include: {
-    I: './steps_file.js'
-  },
-  bootstrap: null,
-  mocha: {},
-  name: 'MakanLezat',
-  plugins: {
-    pauseOnFail: {},
-    retryFailedStep: {
-      enabled: true
+    tests: 'e2e/**/*.spec.js',
+    output: 'e2e/outputs',
+    helpers: {
+        Puppeteer: {
+            url: 'http://localhost:8080',
+            show: true,
+            windowSize: '1200x900',
+        },
     },
-    tryTo: {
-      enabled: true
+    include: {
+        I: './steps_file.js',
     },
-    screenshotOnFail: {
-      enabled: true
-    }
-  }
-}
+    bootstrap: null,
+    mocha: {},
+    name: 'MakanLezat',
+    plugins: {
+        pauseOnFail: {},
+        retryFailedStep: {
+            enabled: true,
+        },
+        tryTo: {
+            enabled: true,
+        },
+        screenshotOnFail: {
+            enabled: true,
+        },
+    },
+};
